3eeff846dd1dae4e662397cd5144247ead0f0c36
seyaz <seji0218@gmail.com>
2021-03-25 11:35:24 +0900

    Branch PAAS-TA-PORTAL-V3-SP3

--- src/main/java/org/openpaas/paasta/portal/web/admin/WebAdminApplication.java
+++ src/main/java/org/openpaas/paasta/portal/web/admin/WebAdminApplication.java
@@ -2,12 +2,9 @@ package org.openpaas.paasta.portal.web.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication // =>@Configuratio,@EnableAutoConfiguration,@ComponentScan 3개의 어노테이션을 묶어놓은것
-//@EnableDiscoveryClient
+@SpringBootApplication
 public class WebAdminApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(WebAdminApplication.class, args);
